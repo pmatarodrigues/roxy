586e8754164a3ab244bf22805fceed436d706a51
Pedro Mata Rodrigues <me@pmatarodrigues.com>
2026-05-19 00:53:46 +0100

    feat(publications): add oral and poster communications section to /publications page

--- src/app/pages/publications-page.component.ts
+++ src/app/pages/publications-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
+import rawCommunications from '../features/publications/communications.json';
 import rawPublications from '../features/publications/publications.json';
 
 interface Publication {
@@ -12,6 +13,20 @@ interface Publication {
   readonly type: 'article' | 'book-chapter' | 'opinion';
   readonly url: string;
   readonly doi: string | null;
+}
+
+interface Communication {
+  readonly id: string;
+  readonly authors: string;
+  readonly title: string;
+  readonly date: string;
+  readonly event: string;
+  readonly location: string;
+}
+
+interface Communications {
+  readonly oral: readonly Communication[];
+  readonly poster: readonly Communication[];
 }
 
 const TYPE_LABELS: Record<Publication['type'], string> = {
@@ -29,6 +44,7 @@ const TYPE_LABELS: Record<Publication['type'], string> = {
 })
 export class PublicationsPageComponent {
   protected readonly publications = rawPublications as Publication[];
+  protected readonly communications = rawCommunications as Communications;
 
   protected getTypeLabel(type: Publication['type']): string {
     return TYPE_LABELS[type];
